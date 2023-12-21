b267d40b9c4d64c6b2717657d9be4ea4ab7dd032
Bo Yan <yan7bo@gmail.com>
2023-12-21 18:55:27 -0500

    Adds dialog input to allow user to add a new book
    Add Book button opens a dialog that prompts to user to input the title
    of a book. When the user clicks Add Book button in the dialog, the page
    will display the updated myLibrary including the new book title.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author="", pages="", read=false) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -29,11 +29,55 @@ function addLibrary(myLibrary) {
     })
 }
 
+
+
 const book1 = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowlings", 10, false);
 myLibrary.push(book1);
 
 const book2 = new Book("Harry Potter and the Chamber of Secrets", "J.K. Rowlings", 12, true);
 myLibrary.push(book2);
 
-console.log(myLibrary);
 addLibrary(myLibrary);
+
+// Adds ability to reset library to reset-library button
+function resetLibrary() {
+    // resets the books displayed on index.html
+    document.querySelectorAll("li").forEach((item) => {
+        item.remove();
+    })
+}
+
+const resetLibraryBtn = document.querySelector("#reset-library");
+resetLibraryBtn.addEventListener("click", resetLibrary);
+
+// Adds ability to display library to add-library button
+const addLibraryBtn = document.querySelector("#add-library");
+addLibraryBtn.addEventListener("click", () => {
+    addLibrary(myLibrary);
+})
+
+// Adding dialog to add books
+const dialog = document.querySelector("dialog");
+const openDialogBtn = document.querySelector("#open-dialog-btn");
+const closeDialogBtn = document.querySelector("#close-btn");
+const addBookBtn = document.querySelector("#add-book-btn");
+const bookInput = document.querySelector("#book");
+
+// show dialog
+openDialogBtn.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+// close dialog
+closeDialogBtn.addEventListener("click", () => {
+    dialog.close();
+})
+
+// submit book
+addBookBtn.addEventListener("click", (event) => {
+    const book = new Book(bookInput.value);
+    myLibrary.push(book);
+    resetLibrary();
+    addLibrary(myLibrary);
+    dialog.close();
+})
